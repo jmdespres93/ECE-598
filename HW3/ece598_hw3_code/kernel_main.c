e1a0700c990313ca2dd70f82cc4bbfd2aa5e97f2
Jasmine Despres <jmdespres93@gmail.com>
2016-02-11 08:08:13 -0500

    put the blinks back in

--- HW3/ece598_hw3_code/kernel_main.c
+++ HW3/ece598_hw3_code/kernel_main.c
@@ -38,18 +38,24 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t *atags) {
 	int input_pointer;
 	int ch;
 
+
 	(void) r0;	/* Ignore boot method */
 
 	/* Initialize Hardware */
 	uart_init();
-
+// Write 1 to GPIO pin 47. make it an output
+	gpio[GPIO_GPFSEL4] = 1<<21;
 	/* Pause so we don't scroll away boot messages before */
 	/* The terminal emulator is ready */
 	printk("Press any key to continue...\r\n");
-
+// Write 1 to GPIO pin 47 to set it. Turns on LED
+	gpio[GPIO_GPSET1] = 1<<15;
 	uart_getc();
+// Write 1 to GPIO pin 47 to clear it. Turns on LED
+	gpio[GPIO_GPCLR1] = 1<<15;
 
 	/* Print boot message */
+//Nyan cat in magenta (make sure color is on)
 	printk(KMAG "+      o     +              o   \r\n    +             o     +       +\r\no          +\r\n    o  +           +        +\r\n+        o     o       +        o\r\n-_-_-_-_-_-_-_,------,      o \r\n_-_-_-_-_-_-_-|   /\\_/\\  \r\n-_-_-_-_-_-_-~|__( ^ .^)  +     +  \r\n");
 	printk("-_-_-_-_-_-_-_\"\" \"\"      \r\n+      o         o   +       o\r\n    +         +\r\no        o         o      o     +\r\n    o           +\r\n+      +     o        o      +    \r\n\r\n" ANSI_COLOR_RESET);
 	printk("%x %x %x", 16, 128, 65535);
@@ -58,6 +64,8 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t *atags) {
 
 	/* Loop forever, echoing what is typed */
 	while (1) {
+		gpio[GPIO_GPSET1] = 1<<15;
 		uart_putc(uart_getc());
+		gpio[GPIO_GPCLR1] = 1<<15;
 	}
 }
