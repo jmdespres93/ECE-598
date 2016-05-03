7814ea498b67705d842676383732976646224701
Jasmine Despres <jmdespres93@gmail.com>
2016-05-03 13:03:11 -0400

    save song in header

--- project/ece598_hw9_code/nyan.c
+++ project/ece598_hw9_code/nyan.c
@@ -2,11 +2,11 @@
 #include <stdint.h>
 #include "bcm2835_periph.h"
 #include "nyan.h"
-
+#include "song.h"
 /** created from binary via objcopy */
-extern uint8_t Sample[]      asm("_binary_Sample_bin_start");
-extern uint8_t Sample_size[] asm("_binary_Sample_bin_size");
-extern uint8_t Sample_end[]  asm("_binary_Sample_bin_end");
+// extern uint8_t Sample[]      asm("_binary_Sample_bin_start");
+// extern uint8_t Sample_size[] asm("_binary_Sample_bin_size");
+// extern uint8_t Sample_end[]  asm("_binary_Sample_bin_end");
 
 void nyan_init(void) {
 	//load GPIO base
@@ -25,15 +25,15 @@ void nyan_init(void) {
 void nyan_sing(void) {
 	//load PWM base
 	uint32_t * pwm  = (uint32_t *)PWM_BASE;
-	uint8_t  * song = Sample;
-
+	uint8_t  * psong = song;
+	uint8_t  * psong_end = &song[1539490];
 	//Check if at end of song
-	while(song!=Sample_end) {
+	while(psong!=psong_end) {
 		//load byte into PWM FIF1
-		pwm[PWM_FIF1] = *song;
+		pwm[PWM_FIF1] = *psong;
 		while(pwm[PWM_STA] && PWM_FULL1);
 		//move to next byte
-		song++;
+		psong++;
 	}
 }
 void nyan_dance(void);
